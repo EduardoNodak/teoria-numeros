f8dd6e465d4016bff97deb8de8a077db1ded4edd
EduardoNodak <eduardohrnodak@gmail.com>
2025-01-27 10:37:06 -0300

    Update extendedEuclides.py

--- extendedEuclides.py
+++ extendedEuclides.py
@@ -1,6 +1,7 @@
-def extendedEuclides(a,b):
-    if b==0:
-        return [1,0,a]
-    else:
-        x,y,d=extendedEuclides(b, a%b)
-        return [y,x-(a//b)*y,d]
+def extendedEuclides(a, b): 
+    if a == 0:
+        return b, 0, 1
+    mdc, x1, y1 = euclides(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return mdc, x, y
